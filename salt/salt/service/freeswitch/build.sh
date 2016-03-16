09e27da4d45584189064238581a041eb201bbf24
Chad Phillips <chad@apartmentlines.com>
2016-03-16 13:15:25 -0600

    rip out dead code.

--- salt/salt/service/freeswitch/build.sh
+++ salt/salt/service/freeswitch/build.sh
@@ -7,10 +7,6 @@
 /usr/bin/perl  -i -pe 's/#applications\/mod_av/applications\/mod_av/g' modules.conf
 /usr/bin/perl  -i -pe 's/#formats\/mod_vlc/formats\/mod_vlc/g' modules.conf
 
-# TODO: Remove this?
-# This was broken for me on build, and it shouldn't impact the video testing.
-#/usr/bin/perl  -i -pe 's/applications\/mod_spandsp/#applications\/mod_spandsp/g' modules.conf
-
 make
 make install
 make sounds-install
