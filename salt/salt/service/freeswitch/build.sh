4581f2cf939d43fc5d8cec512c7e6edf403fde2e
Chad Phillips <chad@apartmentlines.com>
2016-04-06 13:33:50 -0500

    use -C switch for configure, safe and faster.

--- salt/salt/service/freeswitch/build.sh
+++ salt/salt/service/freeswitch/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./bootstrap.sh -j
-./configure
+./configure -C
 
 # Modify modules.conf to add some video deps.
 /usr/bin/perl  -i -pe 's/#applications\/mod_av/applications\/mod_av/g' modules.conf
